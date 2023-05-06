6dfb3c9374ca2ca19463adb5300acbab45f04f7b
tobi <tobibags19@gmail.com>
2023-05-06 14:23:46 -0500

    Created a form model

---
+++ job_application/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class ApplicationForm(forms.Form):
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    date = forms.DateField()
+    occupation = forms.CharField(max_length=80)
--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+            print(first_name)
     return render(request, "index.html")
