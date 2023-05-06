4754ef7c79440c515d6d346226056a29cbd0ca65
tobi <tobibags19@gmail.com>
2023-05-06 15:37:48 -0500

    Implemented base template for multiple webpages

--- job_application/views.py
+++ job_application/views.py
@@ -24,3 +24,7 @@ def index(request):
 
             messages.success(request, "Form successfully submitted!")
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
